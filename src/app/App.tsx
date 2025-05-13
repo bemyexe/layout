324611ee9036a160e321d6c611bf7402f35033b5
maxim <trifonov.max@mail.ru>
2025-05-13 19:16:40 +0300

    base

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,3 @@
-import './global-styles/reset.scss';
-import './global-styles/global.scss';
-import './global-styles/colors.scss';
-
 export const App = () => {
   return <div>App</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import {createRoot} from 'react-dom/client';
 
 import {App} from './app/App';
 
+import './global-styles/reset.scss';
+import './global-styles/global.scss';
+import './global-styles/colors.scss';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
